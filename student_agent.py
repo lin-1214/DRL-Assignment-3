3e7709ddd9e909116f738b574771745edcdcf2fd
lin-1214 <b10901059@g.ntu.edu.tw>
2025-04-22 21:09:05 +0800

    ...

--- student_agent.py
+++ student_agent.py
@@ -9,7 +9,7 @@ import gym
 import cv2
 from gym.wrappers import FrameStack
 
-model_path = 'mario_dueling_dqn.pth'
+model_path = 'best_mario_dueling_dqn.pth'
 
 # Preprocessing wrappers for Mario environment
 class SkipFrame(gym.Wrapper):
