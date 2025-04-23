161e36165d173944b43660a6a839f5ec78c71cd6
lin-1214 <b10901059@g.ntu.edu.tw>
2025-04-24 02:35:43 +0800

    map to cpu

--- student_agent.py
+++ student_agent.py
@@ -137,7 +137,7 @@ class Agent(object):
 
     
     def load(self, path):
-        checkpoint = torch.load(path)
+        checkpoint = torch.load(path, map_location=torch.device('cpu'))
         self.policy_net.load_state_dict(checkpoint['policy_net'])
         self.target_net.load_state_dict(checkpoint['target_net'])
         self.optimizer.load_state_dict(checkpoint['optimizer'])
